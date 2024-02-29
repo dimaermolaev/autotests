86e744684183e8d7252532d91788c54616ecce44
Dmitry Ermolaev <dsermolaev@bug-busters.ru>
2024-02-29 14:22:43 +0300

    added positive and negative tests

---
+++ src/test/java/tests/LoginTestPositive.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class LoginTestPositive {
+
+    @Test
+    public void loginTest() {
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+
+}
