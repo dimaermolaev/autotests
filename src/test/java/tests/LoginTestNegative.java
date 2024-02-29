12fbf9916f91d21b6446b84af80c15e386e9f237
Dmitry Ermolaev <dsermolaev@bug-busters.ru>
2024-02-29 14:26:35 +0300

    small fix

--- src/test/java/tests/LoginTestNegative.java
+++ src/test/java/tests/LoginTestNegative.java
@@ -6,17 +6,16 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
-public class LoginTestPositive {
+public class LoginTestNegative {
     private static final String URL = "https://reqres.in/api/login";
-    private static String email = "eve.holt@reqres.in";
-    private static String password = "cityslicka";
-    private static String expectedToken = "QpwL5tke4Pnpja7X4";
-
+    private static String invalidEmail = "invalid@example.com";
+    private static String invalidPassword = "invalidPassword";
+    private static String expectedErrorMessage = "user not found";
 
     @Test
-    public void loginTest() {
+    public void loginNegativeTest() {
 
-        String data = "{ \"email\": \"" + email + "\", \"password\": \"" + password + "\" }";
+        String data = "{ \"email\": \"" + invalidEmail + "\", \"password\": \"" + invalidPassword + "\" }";
         given()
                 .log().uri()
                 .contentType(JSON)
@@ -26,7 +25,7 @@ public class LoginTestPositive {
                 .then()
                 .log().status()
                 .log().body()
-                .statusCode(200)
-                .body("token", is(expectedToken));
+                .statusCode(400)
+                .body("error", is(expectedErrorMessage));
     }
 }
