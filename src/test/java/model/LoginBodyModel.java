5ac1d89601ce81e2a31b09f42287094533291ec4
Dmitry Ermolaev <dsermolaev@bug-busters.ru>
2024-03-01 12:01:35 +0300

    added Lombok

--- src/test/java/model/LoginBodyModel.java
+++ src/test/java/model/LoginBodyModel.java
@@ -1,21 +1,9 @@
 package model;
 
+import lombok.Data;
+
+@Data
 public class LoginBodyModel {
     String email;
     String password;
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }}
+}
--- src/test/java/model/LoginResponseModel.java
+++ src/test/java/model/LoginResponseModel.java
@@ -1,14 +1,8 @@
 package model;
 
+import lombok.Data;
+@Data
 public class LoginResponseModel {
     String token;
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
 
